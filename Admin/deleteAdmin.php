f765d89aa8e7331713c94def2fbdacba94dc96b6
TrizahNabwire <trizahnabwire20@gmail.com>
2023-10-23 22:18:19 +0300

    Delete Admin Successfull

--- Admin/deleteAdmin.php
+++ Admin/deleteAdmin.php
@@ -1,11 +1,27 @@
 <?php
+session_start();
+    include("../Authentication/connection.php");
 
 // get the id of Admin to be deleted
- echo $id = $_GET['id'];
-
-
+ $id = $_GET['id'];
 
 // Create SQL Query to delete Admin
+$query = "DELETE FROM admin WHERE id = $id";
+
+// Execute Query
+$result = mysqli_query($con, $query);
+
+// check whether Query executed successfully
+
+if ($result==TRUE)
+ {
+    # code...
+    echo "Admin Deleted";
+}else
+ {
+    # code...
+    echo "Failed to Delete Admin";
+}
 
 
 // Redirect to manage Admin page
