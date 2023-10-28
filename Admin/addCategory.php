4030f6496221bc8e6be780cc74cb74267925cc0a
TrizahNabwire <trizahnabwire20@gmail.com>
2023-10-28 18:42:47 +0300

    Image Upload

--- Admin/addCategory.php
+++ Admin/addCategory.php
@@ -55,14 +55,30 @@ session_start();
             echo $_SESSION['addcategory'];
             unset($_SESSION['addcategory']);
         }
+
+        if (isset($_SESSION['upload'])) 
+        {
+            echo $_SESSION['upload'];
+            unset($_SESSION['upload']);
+        }
         ?>
         <br>
     <div class="container">
-    <form method="post">
+        <!--  enable file uploads within a form and ensure that the form data is properly encoded for file uploads, you should include the enctype="multipart/form-data" -->
+    <form method="post" enctype="multipart/form-data">
         <table class="table">
             <tr>
                 <td>Title</td>
                 <td><input type="text" class="form-control" id="titlet" name="title" placeholder="Category Title"></td>
+            </tr>
+            <tr>
+                <td>Select Image</td>
+                <td>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="imageInput" name="image">
+                        <label class="custom-file-label" for="imageInput" name="image">Choose file</label>
+                    </div>
+                </td>
             </tr>
             <tr>
                 <td>Featured</td>
@@ -124,9 +140,41 @@ if (isset($_POST['submit'])) {
         $active = "No";
     }
 
+    // check whether image is selected or not and set the value for the image
+    //print_r($_FILES['image']);
+
+    //die(); //break the echo
+    if (isset($_FILES['image']['name']))
+     {
+        // upload the image
+        // to upload the image we need image_name, source path and destination path
+        $image_name = $_FILES['image']['name'];
+        $source_path = $_FILES['image']['tmp_name'];
+        $destination_path = "../images/category".$image_name;
+
+        // upload the image
+        $upload = move_uploaded_file($source_path,$destination_path);
+
+        // check whether the image is uploaded or not
+        // and if the image is not uploaded then we will stop the process and redirect with error message
+        if ($upload==false)
+         {
+            # set message
+            $_SESSION['upload'] = "<div class='text-danger text-center'>Failed to upload Image.</div>";
+            header("Location: addCategory.php");
+            // stop the process
+            die();
+        }
+
+    }else {
+        // don't upload image and set the image_name value as blank
+        $image_name = "";
+    }
+
     // creating SQL Query to insert Category into database
     $query = "INSERT INTO category SET
     title = '$title',
+    image_name = '$image_name',
     featured = '$featured',
     active = '$active'
     ";
@@ -134,11 +182,12 @@ if (isset($_POST['submit'])) {
     // Executing the Query and saving in Database
     $result = mysqli_query($con,$query);
 
-    if ($result==true) {
+    if ($result==true)
+     {
         $_SESSION['addcategory'] = "<div class='text-success text-center'>Category Added Succesfully</div>";
         header("Location: manageCategory.php");
     }else {
-        $_SESSION['addcategory'] = "<div class='text-danger text-center'>Failed to AddCategory Added Succesfully</div>";
+        $_SESSION['addcategory'] = "<div class='text-danger text-center'>Failed to Add Category</div>";
         header("Location: addCategory.php");
     }
 
